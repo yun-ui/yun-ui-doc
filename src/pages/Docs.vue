1781c0e1aa1ffe31535e40768fd108f73207cb9b
yueyu_li <yueyu_li@kingdee.com>
2016-11-28 19:54:52 +0800

    add demoArea page CSS

--- src/pages/Docs.vue
+++ src/pages/Docs.vue
@@ -1,19 +1,21 @@
 <template>
     <section class="page page-doc">
         <left-side-bar></left-side-bar>
-        <main-doc-area><router-view></router-view></main-doc-area>
+        <main-doc-area>
+            <router-view></router-view>
+        </main-doc-area>
         <demo-area></demo-area>
     </section>
 </template>
 
 <script>
-import { LeftSideBar, MainDocArea, DemoArea } from 'components'
+    import {LeftSideBar, MainDocArea, DemoArea} from 'components'
 
-export default {
-    components: {
-        LeftSideBar,
-        MainDocArea,
-        DemoArea
+    export default {
+        components: {
+            LeftSideBar,
+            MainDocArea,
+            DemoArea
+        }
     }
-}
 </script>
