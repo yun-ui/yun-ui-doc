6d8795445e7bcf0dce4c4eb3bbb6eac32156521f
aleksichen <aleksichen@gmail.com>
2016-12-24 11:54:38 +0800

    test ci

--- src/menuList.js
+++ src/menuList.js
@@ -4,21 +4,21 @@ export default [
         secondary: 'Update Log',
         subMenu: [],
         routePath: 'changelog',
-        component: resolve => require(['docs/README3.md'], resolve)
+        component: resolve => require(['./documents/README.md'], resolve)
     },
     {
         title: '没有子集没路由',
         secondary: 'Update Log',
         subMenu: [],
         routePath: 'cs',
-        component: resolve => require(['docs/README2.md'], resolve)
+        component: resolve => require(['./documents/README.md'], resolve)
     },
     {
         title: '没有子集有路由',
         secondary: 'Update Log',
         subMenu: [],
         routePath: 'b',
-        component: resolve => require(['docs/README1.md'], resolve)
+        component: resolve => require(['./documents/README.md'], resolve)
     },
     {
         title: '基础组件',
@@ -29,31 +29,31 @@ export default [
                 title: '布局',
                 secondary: 'Layout',
                 routePath: 'layout',
-                component: resolve => require(['docs/README3.md'], resolve)
+                component: resolve => require(['./documents/README.md'], resolve)
             },
             {
                 title: '颜色',
                 secondary: 'Color',
                 routePath: 'color',
-                component: resolve => require(['docs/README3.md'], resolve)
+                component: resolve => require(['./documents/README.md'], resolve)
             },
             {
                 title: '字体',
                 secondary: 'Update Log',
                 routePath: 'font',
-                component: resolve => require(['docs/README1.md'], resolve)
+                component: resolve => require(['./documents/README.md'], resolve)
             },
             {
                 title: '图标',
                 secondary: 'Update Log',
                 routePath: 'icons',
-                component: resolve => require(['docs/README3.md'], resolve)
+                component: resolve => require(['./documents/README.md'], resolve)
             },
             {
                 title: '按钮',
                 secondary: 'Update Log',
                 routePath: 'buttons',
-                component: resolve => require(['docs/README3.md'], resolve)
+                component: resolve => require(['./documents/README.md'], resolve)
             }
         ]
     }
